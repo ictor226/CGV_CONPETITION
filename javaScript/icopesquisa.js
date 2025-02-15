b7edb0fdc1ec863bf87ceda35ee3f94aaa607fdc
Victor Huggo <huggoV226@gmail.com>
2025-02-14 23:33:27 -0300

    arrumamos o inicio da tela

--- javaScript/icopesquisa.js
+++ javaScript/icopesquisa.js
@@ -1,11 +1,8 @@
+// Seleciona o ícone de pesquisa e a barra de pesquisa
+const searchIcon = document.getElementById('search-icon');
+const searchBar = document.getElementById('search-bar');
 
-        // Seleciona o ícone de pesquisa e a barra de pesquisa
-        const searchIcon = document.getElementById('search-icon');
-        const searchBar = document.getElementById('search-bar');
-
-        // Adiciona o evento de clique no ícone
-        searchIcon.addEventListener('click', function() {
-            // Alterna a visibilidade e a largura da barra de pesquisa
-            searchBar.classList.toggle('show');
-            searchBar.focus(); // Foca na barra de pesquisa quando ela é expandida
-        });
+// Adiciona o evento de clique no ícone de pesquisa
+searchIcon.addEventListener('click', function() {
+    searchBar.classList.toggle('show'); // Alterna a classe "show" para abrir ou fechar a barra
+});
