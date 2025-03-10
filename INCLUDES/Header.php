7b6eec88bbb3d520f1f84c412b9d17f8486874af
caua <gustavo.senac20@gmail.com>
2025-03-10 11:54:46 -0300

    header e index

--- INCLUDES/Header.php
+++ INCLUDES/Header.php
@@ -22,43 +22,39 @@
     </title>
 
 </head>
+
+<body>
 <header>
     <nav>
-        <button class="menu-oculto" onclick="javascript:abrirNav()">
-            <i class="bi bi-list"></i>
-        </button>
+        <a href="./index.php" class="logo">
+            <h1>CGV COMPETITION</h1>
+        </a>
         <ul class="menu">
-            <li><a href="./index.php">Inicio</a></li>
+            <li><a href="./index.php">Início</a></li>
             <li><a href="#">Sobre</a></li>
             <li><a href="#">Produtos</a></li>
         </ul>
-        <div class="search-container">
-            <input type="text" id="search-bar" placeholder="Buscar produtos...">
-            <button id="search-icon" onclick="toggleSearch()">
-                <i class="bi bi-search"></i>
-            </button>
-        </div>
+
         <ul class="menu-icones">
+            <li><a href="#"><i class="bi bi-search"></i></a></li>
             <li><a href="./cart.php"><i class="bi bi-cart"></i></a></li>
             <li><a href="./tela-login.php"><i class="bi bi-person-circle"></i></a></li>
         </ul>
-    </nav>
 
-    <button class="menu-oculto" onclick="Javascript:abrirNav()">
-
-    </button>
-
-    <div id="offcanvas" class="menu-oculto">
-        <button class="fechar" onclick="Javascript:fecharNav()">
-            <i class="bi bi-x"></i>
-
-            <a href="./cad.php">cadastrar produto</a>
-            <a href="#">Exemplo</a>
-            <a href="#">Exemplo</a>
-
-            <hr>
-
+        <!-- Botão para abrir o menu oculto -->
+        <button class="menu-oculto" onclick="Javascript:abrirNav()">
+            <i class="bi bi-list"></i>
         </button>
-    </div>
 
+        <!-- Menu oculto -->
+        <div id="offcanvas" class="menu-oculto">
+            <button class="fechar" onclick="Javascript:fecharNav()">
+                <i class="bi bi-x"></i>
+            </button>
+            <li><a href="./index.php">Início</a></li>
+            <li><a href="#">Sobre</a></li>
+            <li><a href="#">Produtos</a></li>
+        </div>
+    </nav>
 </header>
+</body>
--- javaScript/offCanvas.js
+++ javaScript/offCanvas.js
@@ -1,10 +1,9 @@
-function abrirNav(){
-    const menu = document.getElementById("offcanvas")
-    menu.style.width = "250px"
+function abrirNav() {
+    const menu = document.getElementById("offcanvas");
+    menu.style.width = "250px"; // Abre o menu
 }
- 
-function fecharNav(){
- 
-    const menu = document.getElementById("offcanvas")
-    menu.style.width = "0"
+
+function fecharNav() {
+    const menu = document.getElementById("offcanvas");
+    menu.style.width = "0"; // Fecha o menu
 }
