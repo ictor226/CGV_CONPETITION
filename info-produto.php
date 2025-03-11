619314a49ac968dafda20cafedc16e345486a141
caua <gustavo.senac20@gmail.com>
2025-03-11 11:12:06 -0300

    atualização

---
+++ info-produto.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="./ASSETS/javaScript/troca-img.js" defer></script> <!-- Certifique-se de que o caminho está correto -->
+</head>
+<body>
+
+    <?php
+    include './includes/header.php';
+    ?>
+
+    <img src="./ASSETS/img/produtos/roda.png" alt="foto produto" id="figura">
+    <button onclick="troca()"><i class="bi bi-arrow-right-short"></i></button>
+
+</body>
+</html>
