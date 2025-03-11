5dfea13dda5a4e62a3d3fc0f128438e8d58ec618
caua <gustavo.senac20@gmail.com>
2025-03-11 09:00:19 -0300

    include footer

---
+++ INCLUDES/footer.php
@@ -0,0 +1,36 @@
+<img src="./ASSETS/img/tela inicial/rodape.png" alt="Rodapé" class="img-rodape-02">
+
+<footer>
+    <section class="info">
+        <div class="social-media">
+            <h5>REDES SOCIAIS</h5>
+            <a href="#" class="social-icon"><i class="bi bi-instagram"></i> Instagram</a>
+            <a href="#" class="social-icon"><i class="bi bi-google"></i> Google</a>
+            <a href="#" class="social-icon"><i class="bi bi-facebook"></i> Facebook</a>
+        </div>
+
+        <div class="customer-support">
+            <h5>SUPORTE AO CLIENTE</h5>
+            <a href="#" class="support-link"><i class="bi bi-chat-left-dots-fill"></i> Entre em contato</a>
+            <a href="#" class="support-link"><i class="bi bi-telephone-fill"></i> Telefone - Suporte ao cliente e Dúvidas</a>
+            <a href="#" class="support-link"><i class="bi bi-whatsapp"></i> WhatsApp - Suporte ao cliente e Dúvidas</a>
+        </div>
+
+        <div class="quick-links">
+            <h5>LINKS RÁPIDOS</h5>
+            <a href="#">Política de privacidade</a>
+            <a href="#">Termos de Uso</a>
+            <a href="#">Política de Devolução e Troca</a>
+        </div>
+
+        <div class="newsletter">
+            <h5>NOTÍCIAS E ATUALIZAÇÕES</h5>
+            <p>Inscreva-se agora com o e-mail para receber as novidades da loja em tempo real!</p>
+            <input type="email" placeholder="Digite seu e-mail" class="email-input">
+            <button class="submit-btn">Enviar</button>
+        </div>
+    </section>
+</footer>
+
+<script src="./javaScript/icopesquisa.js"></script>
+<script src="./javaScript/offCanvas.js"></script>
--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
-    include './INCLUDES/Header.php';
-             
+include './INCLUDES/Header.php';
+
 ?>
+
 <body>
-<div class="background-site">
-    
+    <div class="background-site">
+
 
         <div class="video-inicio" utoplay loop muted playsinline>
             <figure class="video-container">
@@ -13,212 +14,148 @@
                 </video>
             </figure>
         </div>
-        <!-- <iframe src="./ASSETS/img/tela inicial/CGV COMPETITON.mp4" frameborder="0"></iframe> -->
+        </header>
+        </main>
 
-    </header>
-    </main>
+        <h1 class="produ">PRODUTOS EM DESTAQUE</h1>
 
-
-
-
-
-    <!-- -----------------------------segunda tela---------------------------------------->
-
-
-    <!-- Conteúdo que ficará sobre a imagem -->
-   
-    
-    <h1 class="produ">PRODUTOS EM DESTAQUE</h1>
-
-    <section class="produtos">
-        <div class="container">
-            <div class="item">
-                <figure>
-                    <img src="./ASSETS/img/tela inicial/nitro.png" alt="KIT NITRO NOS COMPLETO" class="foto-produto">
-                    <figcaption>
-                        <h4 class="h4-tela02">KIT NITRO NOS COMPLETO</h4>
-                    </figcaption>
-                </figure>
+        <section class="produtos">
+            <div class="container">
+                <div class="item">
+                    <figure>
+                        <img src="./ASSETS/img/tela inicial/nitro.png" alt="KIT NITRO NOS COMPLETO" class="foto-produto">
+                        <figcaption>
+                            <h4 class="h4-tela02">KIT NITRO NOS COMPLETO</h4>
+                        </figcaption>
+                    </figure>
+                </div>
+                <div class="item">
+                    <figure>
+                        <img src="./ASSETS/img/tela inicial/kit turbo.png" alt="KIT NITRO NOS COMPLETO"
+                            class="foto-produto">
+                        <figcaption>
+                            <h4 class="h4-tela02">KIT NITRO NOS COMPLETO</h4>
+                        </figcaption>
+                    </figure>
+                </div>
+                <div class="item">
+                    <figure>
+                        <img src="./ASSETS/img/tela inicial/turbo.png" alt="KIT NITRO NOS COMPLETO" class="foto-produto">
+                        <figcaption>
+                            <h4 class="h4-tela02">KIT NITRO NOS COMPLETO</h4>
+                        </figcaption>
+                    </figure>
+                </div>
+                <div class="item">
+                    <figure>
+                        <img src="./ASSETS/img/tela inicial/roda.png" alt="KIT NITRO NOS COMPLETO" class="foto-produto">
+                        <figcaption>
+                            <h4 class="h4-tela02">KIT NITRO NOS COMPLETO</h4>
+                        </figcaption>
+                    </figure>
+                </div>
             </div>
-            <div class="item">
-                <figure>
-                    <img src="./ASSETS/img/tela inicial/kit turbo.png" alt="KIT NITRO NOS COMPLETO"
-                        class="foto-produto">
-                    <figcaption>
-                        <h4 class="h4-tela02">KIT NITRO NOS COMPLETO</h4>
-                    </figcaption>
-                </figure>
+
+            <div class="container02">
+                <div class="item02">
+                    <figure>
+                        <img src="./ASSETS/img/tela inicial/nitro.png" alt="KIT NITRO NOS COMPLETO" class="foto-produto">
+                        <figcaption>
+                            <h4 class="h4-tela02">KIT NITRO NOS COMPLETO</h4>
+                        </figcaption>
+                    </figure>
+                </div>
+                <div class="item02">
+                    <figure>
+                        <img src="./ASSETS/img/tela inicial/kit turbo.png" alt="KIT NITRO NOS COMPLETO"
+                            class="foto-produto">
+                        <figcaption>
+                            <h4 class="h4-tela02">KIT NITRO NOS COMPLETO</h4>
+                        </figcaption>
+                    </figure>
+                </div>
+                <div class="item02">
+                    <figure>
+                        <img src="./ASSETS/img/tela inicial/turbo.png" alt="KIT NITRO NOS COMPLETO" class="foto-produto">
+                        <figcaption>
+                            <h4 class="h4-tela02">KIT NITRO NOS COMPLETO</h4>
+                        </figcaption>
+                    </figure>
+                </div>
+                <div class="item02">
+                    <figure>
+                        <img src="./ASSETS/img/tela inicial/roda.png" alt="KIT NITRO NOS COMPLETO" class="foto-produto">
+                        <figcaption>
+                            <h4 class="h4-tela02">KIT NITRO NOS COMPLETO</h4>
+                        </figcaption>
+                    </figure>
+                </div>
             </div>
-            <div class="item">
-                <figure>
-                    <img src="./ASSETS/img/tela inicial/turbo.png" alt="KIT NITRO NOS COMPLETO" class="foto-produto">
-                    <figcaption>
-                        <h4 class="h4-tela02">KIT NITRO NOS COMPLETO</h4>
-                    </figcaption>
-                </figure>
+
+            <div class="container03">
+                <div class="item03">
+                    <figure>
+                        <img src="./ASSETS/img/tela inicial/nitro.png" alt="KIT NITRO NOS COMPLETO" class="foto-produto">
+                        <figcaption>
+                            <h4 class="h4-tela02">KIT NITRO NOS COMPLETO</h4>
+                        </figcaption>
+                    </figure>
+                </div>
+                <div class="item03">
+                    <figure>
+                        <img src="./ASSETS/img/tela inicial/kit turbo.png" alt="KIT NITRO NOS COMPLETO"
+                            class="foto-produto">
+                        <figcaption>
+                            <h4 class="h4-tela02">KIT NITRO NOS COMPLETO</h4>
+                        </figcaption>
+                    </figure>
+                </div>
+                <div class="item03">
+                    <figure>
+                        <img src="./ASSETS/img/tela inicial/turbo.png" alt="KIT NITRO NOS COMPLETO" class="foto-produto">
+                        <figcaption>
+                            <h4 class="h4-tela02">KIT NITRO NOS COMPLETO</h4>
+                        </figcaption>
+                    </figure>
+                </div>
+                <div class="item03">
+                    <figure>
+                        <img src="./ASSETS/img/tela inicial/roda.png" alt="KIT NITRO NOS COMPLETO" class="foto-produto">
+                        <figcaption>
+                            <h4 class="h4-tela02">KIT NITRO NOS COMPLETO</h4>
+                        </figcaption>
+                    </figure>
+                </div>
             </div>
-            <div class="item">
-                <figure>
-                    <img src="./ASSETS/img/tela inicial/roda.png" alt="KIT NITRO NOS COMPLETO" class="foto-produto">
-                    <figcaption>
-                        <h4 class="h4-tela02">KIT NITRO NOS COMPLETO</h4>
-                    </figcaption>
-                </figure>
-            </div>
+        </section>
+
+        <!-- TELA 03 -->
+        <hr class="hr-tela03">
+        <section class="tela-envio">
+            <img src="./ASSETS/img/tela inicial/envio.png" class="img-envio">
+            <h2 class="h2-tela-03">ENVIO IMEDIATO!</h2>
+            <h4 class="h4-tela-03">Isso mesmo! Nós temos envio imediato para a entrega a partir do pagamento realizado na
+                compra de cada item, isso tudo para passar confiança para os nossos clientes, e para mostrar a nossa
+                qualidade impecavél desde o site, até mesmo no item chego em casa!
+            </h4>
+
+        </section>
+        <!-- tla 04 -->
+        <hr class="hr-tela04">
+
+        <img src="./ASSETS/img/tela inicial/sobre nós.png" alt="" class="img-sobre">
+        <div class="dit-h">
+            <h2 class="h2-tela04">SOBRE NÓS</h2>
+
+            <h6 id="h6-tela04">Somos a maior loja de Peças automotivas para super carros do mundo todo! Temos parceria com
+                uma das maiores preparadoras de carros de todos os tempos, a famosa G-POWER, todas as modificações com as
+                melhores peças do mercado saem da nossa empresa!</h6>
         </div>
-
-        <div class="container02">
-            <div class="item02">
-                <figure>
-                    <img src="./ASSETS/img/tela inicial/nitro.png" alt="KIT NITRO NOS COMPLETO" class="foto-produto">
-                    <figcaption>
-                        <h4 class="h4-tela02">KIT NITRO NOS COMPLETO</h4>
-                    </figcaption>
-                </figure>
-            </div>
-            <div class="item02">
-                <figure>
-                    <img src="./ASSETS/img/tela inicial/kit turbo.png" alt="KIT NITRO NOS COMPLETO"
-                        class="foto-produto">
-                    <figcaption>
-                        <h4 class="h4-tela02">KIT NITRO NOS COMPLETO</h4>
-                    </figcaption>
-                </figure>
-            </div>
-            <div class="item02">
-                <figure>
-                    <img src="./ASSETS/img/tela inicial/turbo.png" alt="KIT NITRO NOS COMPLETO" class="foto-produto">
-                    <figcaption>
-                        <h4 class="h4-tela02">KIT NITRO NOS COMPLETO</h4>
-                    </figcaption>
-                </figure>
-            </div>
-            <div class="item02">
-                <figure>
-                    <img src="./ASSETS/img/tela inicial/roda.png" alt="KIT NITRO NOS COMPLETO" class="foto-produto">
-                    <figcaption>
-                        <h4 class="h4-tela02">KIT NITRO NOS COMPLETO</h4>
-                    </figcaption>
-                </figure>
-            </div>
-        </div>
-
-        <div class="container03">
-            <div class="item03">
-                <figure>
-                    <img src="./ASSETS/img/tela inicial/nitro.png" alt="KIT NITRO NOS COMPLETO" class="foto-produto">
-                    <figcaption>
-                        <h4 class="h4-tela02">KIT NITRO NOS COMPLETO</h4>
-                    </figcaption>
-                </figure>
-            </div>
-            <div class="item03">
-                <figure>
-                    <img src="./ASSETS/img/tela inicial/kit turbo.png" alt="KIT NITRO NOS COMPLETO"
-                        class="foto-produto">
-                    <figcaption>
-                        <h4 class="h4-tela02">KIT NITRO NOS COMPLETO</h4>
-                    </figcaption>
-                </figure>
-            </div>
-            <div class="item03">
-                <figure>
-                    <img src="./ASSETS/img/tela inicial/turbo.png" alt="KIT NITRO NOS COMPLETO" class="foto-produto">
-                    <figcaption>
-                        <h4 class="h4-tela02">KIT NITRO NOS COMPLETO</h4>
-                    </figcaption>
-                </figure>
-            </div>
-            <div class="item03">
-                <figure>
-                    <img src="./ASSETS/img/tela inicial/roda.png" alt="KIT NITRO NOS COMPLETO" class="foto-produto">
-                    <figcaption>
-                        <h4 class="h4-tela02">KIT NITRO NOS COMPLETO</h4>
-                    </figcaption>
-                </figure>
-            </div>
-        </div>
-    </section>
-    
-    <!-- TELA 03 -->
-    <hr class="hr-tela03">
-    <section class="tela-envio">
-        <img src="./ASSETS/img/tela inicial/envio.png" class="img-envio">
-        <h2 class="h2-tela-03">ENVIO IMEDIATO!</h2>
-        <h4 class="h4-tela-03">Isso mesmo! Nós temos envio imediato para a entrega a partir do pagamento realizado na
-            compra de cada item, isso tudo para passar confiança para os nossos clientes, e para mostrar a nossa
-            qualidade impecavél desde o site, até mesmo no item chego em casa!
-        </h4>
-
-    </section>
-    <!-- tla 04 -->
-    <hr class="hr-tela04">
-
-    <img src="./ASSETS/img/tela inicial/sobre nós.png" alt="" class="img-sobre">
-    <div class="dit-h">
-        <h2 class="h2-tela04">SOBRE NÓS</h2>
-
-        <h6 id="h6-tela04">Somos a maior loja de Peças automotivas para super carros do mundo todo! Temos parceria com
-            uma das maiores preparadoras de carros de todos os tempos, a famosa G-POWER, todas as modificações com as
-            melhores peças do mercado saem da nossa empresa!</h6>
     </div>
 
-    <!-- tela 05 -->
-
-
-    <img src="./ASSETS/img/tela inicial/rodape.png" alt="" class="img-rodape-02">
-
-
-
-
-
-    <section class="info">
-        <div>
-            <h5>REDES SOCIAIS</h5>
-            <br>
-            <a id="edt-icons" href="#"><i class="bi bi-instagram"> instagram</i></a>
-            <br>
-            <a id="edt-icons" href="#"><i class="bi bi-google"> google</i></a>
-            <br>
-            <a id="edt-icons" href="#"><i class="bi bi-facebook"> facebook</i></a>
-        </div>
-
-        <div>
-            <h5>SUPORTE AO CLIENTE</h5>
-            <br>
-            <a id="edt-icons" href=""><i class="bi bi-chat-left-dots-fill">Entre em contato </i></a>
-            <br>
-            <a id="edt-icons" href=""><i class="bi bi-telephone-fill">Telefone - Suporte ao cliente e Dúvidas</i></a>
-            <br>
-            <a id="edt-icons" href="" <i class="bi bi-whatsapp">Whatsapp - Suporte ao cliente e Dúvidas</i></a>
-        </div>
-
-        <div>
-            <h5>LINKS RÁPIDOS</h5>
-            <br>
-            <a>Política de privacidade</a>
-            <br>
-            <a>Termos de Uso</a>
-            <br>
-            <a>Política de Devolução e Troca</a>
-        </div>
-
-        <div>
-            <h5>NOTÍCIAS E ATUALIZAÇÕES</h5>
-            <br>
-            <a>Inscreva-se agora com o Email de escolha para receber todas as novidades da loja em tempo real!</a>
-            <br>
-            <a><input type="text" placeholder="manssagem" class="espacamento-02"></a>
-            <br>
-
-            <a><button>enviar</button></a>
-        </div>
-
-    </section>
-
-    </div>
+    <?php
+ include './includes/footer.php';
+    ?>
 
 </body>
-<script src="./javaScript/icopesquisa.js"></script>
-<script src="./javaScript/offCanvas.js"></script>
 </html>
