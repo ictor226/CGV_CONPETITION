1ef1b479b757a39165d678f0f37d37d04ccd9365
caua <gustavo.senac20@gmail.com>
2025-03-11 09:00:19 -0300

    include footer

--- INCLUDES/Header.php
+++ INCLUDES/Header.php
@@ -2,59 +2,60 @@
 <html lang="pt-br">
 
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>
+ 
+        <?php
+        if (isset($titulo) && !empty($titulo)) {
+            echo $titulo;
+        } else {
+        echo 'CGV COMPETITION';
+        }
+
+        ?>
+    </title>
+
     <link rel="stylesheet" href="./ASSETS/CSS/bara-pesquisa.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./ASSETS/CSS/tela-inicial.css">
     <link rel="stylesheet" href="./ASSETS/CSS/cart.css">
     <link rel="stylesheet" href="./ASSETS/CSS/header.css">
-
-    <title>
-        <?php
-        if (isset($titulo) &&  !empty($titulo)) {
-            echo $titulo;
-        } else {
-
-            echo 'CGV COMPETITION';
-        }
-        ?>
-
-    </title>
+    <link rel="stylesheet" href="./ASSETS/CSS/footer.css">
 
 </head>
 
 <body>
-<header>
-    <nav>
-        <a href="./index.php" class="logo">
-            <h1>CGV COMPETITION</h1>
-        </a>
-        <ul class="menu">
-            <li><a href="./index.php">Início</a></li>
-            <li><a href="#">Sobre</a></li>
-            <li><a href="#">Produtos</a></li>
-        </ul>
+    <header>
+        <nav>
+            <ul class="menu">
+                <li><a href="./index.php">Início</a></li>
+                <li><a href="#">Sobre</a></li>
+                <li><a href="#">Produtos</a></li>
+            </ul>
 
-        <ul class="menu-icones">
-            <li><a href="#"><i class="bi bi-search"></i></a></li>
-            <li><a href="./cart.php"><i class="bi bi-cart"></i></a></li>
-            <li><a href="./tela-login.php"><i class="bi bi-person-circle"></i></a></li>
-        </ul>
+            <ul class="menu-icones">
+                <li><a href="#"><i class="bi bi-search"></i></a></li>
+                <li><a href="./cart.php"><i class="bi bi-cart"></i></a></li>
+                <li><a href="./tela-login.php"><i class="bi bi-person-circle"></i></a></li>
+            </ul>
 
-        <!-- Botão para abrir o menu oculto -->
-        <button class="menu-oculto" onclick="Javascript:abrirNav()">
-            <i class="bi bi-list"></i>
-        </button>
-
-        <!-- Menu oculto -->
-        <div id="offcanvas" class="menu-oculto">
-            <button class="fechar" onclick="Javascript:fecharNav()">
-                <i class="bi bi-x"></i>
+            <!-- Botão para abrir o menu oculto -->
+            <button class="menu-oculto" onclick="Javascript:abrirNav()">
+                <i class="bi bi-list"></i>
             </button>
-            <li><a href="./index.php">Início</a></li>
-            <li><a href="#">Sobre</a></li>
-            <li><a href="#">Produtos</a></li>
-        </div>
-    </nav>
-</header>
+
+            <!-- Menu oculto -->
+            <div id="offcanvas" class="menu-oculto">
+                <button class="fechar" onclick="Javascript:fecharNav()">
+                    <i class="bi bi-x"></i>
+                </button>
+                <li><a href="./index.php">Início</a></li>
+                <li><a href="#">Sobre</a></li>
+                <li><a href="#">Produtos</a></li>
+                <hr>
+                <li><a href="./cad.php">Cadastrar Produto</a></li>
+            </div>
+        </nav>
+    </header>
 </body>
