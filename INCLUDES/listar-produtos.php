09b2d8d79df42ebae6ec2dd6d95f6d33d935541f
GustavoSobrera20 <gustavo.senac20@gmail.com>
2025-03-24 12:51:24 -0300

    Add files via upload

---
+++ INCLUDES/listar-produtos.php
@@ -0,0 +1,9 @@
+<main class="container">
+        <div class="row">
+ 
+            <?php for ($i = 0; $i < 16; $i++) {
+                include './INCLUDES/cards.php';
+            } ?>
+ 
+        </div>
+    </main>
