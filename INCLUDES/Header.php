0775b90b82add3abeaf208dabf6474efdbc5a9e2
Caua <caua.cferreira2@gmail.com>
2025-03-24 09:28:02 -0300

    refiz o header e footer

--- INCLUDES/Header.php
+++ INCLUDES/Header.php
@@ -2,15 +2,15 @@
 <html lang="pt-br">
 
 <head>
-<meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>
- 
+
         <?php
         if (isset($titulo) && !empty($titulo)) {
             echo $titulo;
         } else {
-        echo 'CGV COMPETITION';
+            echo 'CGV COMPETITION';
         }
 
         ?>
@@ -24,35 +24,14 @@
     <link rel="stylesheet" href="./ASSETS/CSS/footer.css">
     <link rel="stylesheet" href="./ASSETS/CSS/info-produto.css">
     <link rel="stylesheet" href="./ASSETS/css/bara-pesquisa">
-    
+
 </head>
 
-<body>
-    <header>
-        <nav>
-            <ul class="menu">
-                <li><a href="./index.php">Início</a></li>
-                <li><a href="#sobre">Sobre</a></li>
-                <li><a href="#">Produtos</a></li>
-            </ul>
 
-            <ul class="menu-icones">
-            <li>
-    <a href="#" onclick="toggleSearch(event)">
-        <i class="bi bi-search"></i>
-    </a>
-
-    <div id="search-bar" class="search-bar">
-        <input type="text" placeholder="Pesquisar...">
-    </div>
-</li>
-
-                <li><a href="./cart.php"><i class="bi bi-cart"></i></a></li>
-                <li><a href="./tela-login.php"><i class="bi bi-person-circle"></i></a></li>
-            </ul>
-
-            <!-- Botão para abrir o menu oculto -->
-            <button class="menu-oculto" onclick="Javascript:abrirNav()">
+<header>
+    <nav>
+         <!-- Botão para abrir o menu oculto -->
+         <button class="menu-oculto" onclick="Javascript:abrirNav()">
                 <i class="bi bi-list"></i>
             </button>
 
@@ -67,7 +46,30 @@
                 <hr>
                 <li><a href="./cad.php">Cadastrar Produto</a></li>
             </div>
-        </nav>
-    </header>
-    <script src="./JavaScript/icopesquisa.js"></script>
-</body>
+
+        <ul class="menu">
+            <li><a href="./index.php">Início</a></li>
+            <li><a href="#sobre">Sobre</a></li>
+            <li><a href="#">Produtos</a></li>
+        </ul>
+
+        <li>
+            <a href="#" onclick="toggleSearch(event)">
+                <i class="bi bi-search"></i>
+            </a>
+
+            <div id="search-bar" class="search-bar">
+                <input type="text" placeholder="Pesquisar...">
+            </div>
+        </li>
+
+        <li><a href="./cart.php"><i class="bi bi-cart"></i></a></li>
+        <li><a href="./tela-login.php"><i class="bi bi-person-circle"></i></a></li>
+
+
+
+    </nav>
+</header>
+
+
+<script src="./JavaScript/icopesquisa.js"></script>
