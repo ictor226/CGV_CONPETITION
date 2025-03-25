b959a7dc7658261475e94426d2720ea3eabb8d65
caua <gustavo.senac20@gmail.com>
2025-03-25 11:59:41 -0300

    comit real

--- INCLUDES/cards.php
+++ INCLUDES/cards.php
@@ -11,23 +11,22 @@ $select = 'SELECT * FROM cadastro_produto ORDER BY RAND()';
 $resultado = $banco->query($select)->fetchAll();
  
 ?>
-<?php foreach ($resultado as $linha) { ?>
-    <div class="container">
-    <section class="produtos" >
-        <a href="./info-produto.php?id=<?=$linha['id_produto'] ?>">
-            <div class="container">
-               <a href="./info-produto.php"><button class="btn-produto-card">
-                    <div class="item">
-                        <figure>
-                            <img src="./ASSETS/img/produtos/<?php echo $linha ['img_produto1']?>" alt="KIT NITRO NOS COMPLETO" class="foto-produto">
-                            <figcaption>
-                                <h4 class="h4-tela02"><?php echo $linha ['nome_item']  ?></h4>
-                            </figcaption>
-                        </figure>
-                    </div>
-                </button></a> 
-            </div>
+<div class="container">
+  <?php foreach ($resultado as $linha) { ?>
+    <section class="produtos">
+      <a href="./info-produto.php?id=<?=$linha['id_produto'] ?>">
+        <button class="btn-produto-card">
+          <div class="item">
+            <figure>
+              <img src="./ASSETS/img/produtos/<?php echo $linha['img_produto1']?>" alt="KIT NITRO NOS COMPLETO" class="foto-produto">
+              <figcaption>
+                <h4 class="h4-tela02"><?php echo $linha['nome_item'] ?></h4>
+              </figcaption>
+            </figure>
+          </div>
+        </button>
+      </a>
+    </section>
+  <?php } ?>
+</div>
 
-        </section>
-    </div>
-<?php } ?>
