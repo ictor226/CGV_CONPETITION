f083231763d4423780e0d5a3f030c2e9b9196c55
Caua <caua.cferreira2@gmail.com>
2025-04-03 11:55:10 -0300

    falta filtrar so para adm o botao de cadastrar

--- tela-login.php
+++ tela-login.php
@@ -1,27 +1,38 @@
 <?php
 session_start();
+
 if (isset($_SESSION['id_pessoa'])) {
     header("Location: tela-user.php");  // Redireciona caso o usuário já esteja logado
     exit;
 }
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $email = $_POST['email'];
     $senha = $_POST['senha'];
-    // Conectar ao banco de dados
-    $conexao = new mysqli('localhost', 'root', '', 'bd_cgv');
-    
+
+    $dsn = 'mysql:dbname=bd_cgv;host=127.0.0.1'; // Nome do banco e host
+    $user = 'root'; // Usuário
+    $password = ''; // Senha (caso tenha)
+    $banco = new PDO($dsn, $user, $password);
+
     if ($conexao->connect_error) {
         die('Falha na conexão: ' . $conexao->connect_error);
     }
+
     // Preparar a consulta para buscar o usuário
     $sql = "SELECT * FROM login WHERE email = ? AND senha = ?";
     $stmt = $conexao->prepare($sql);
     $stmt->bind_param('ss', $email, $senha); // Senha sem MD5
     $stmt->execute();
     $result = $stmt->get_result();
+
+
     if ($result->num_rows === 1) {
         $usuario = $result->fetch_assoc();
+
         $_SESSION['id_pessoa'] = $usuario['id'];  // Armazena o ID do usuário na sessão
+        $_SESSION['email'] = $usuario['email'];
+
         header("Location: tela-user.php");  // Redireciona para a página principal
         exit;
     } else {
@@ -32,7 +43,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 }
 ?>
 
-
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -42,7 +52,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <title>Document</title>
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    
+
     <link rel="stylesheet" href="./ASSETS/CSS/tela-login.css">
     <link rel="stylesheet" href="./ASSETS/CSS/olhar-senha.css">
     <link rel="stylesheet" href="./javaScript/olhar-senha.js">
@@ -55,32 +65,33 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     </ul>
 
 
-    <form action="./user-login.php"  method="POST">
+    <form action="./user-login.php" method="POST">
 
-<div class="estrutura">
-    <div class="login-form">
-        <h2 class="titulo-login">Login</h2>
-        <h4>Não tem login? <a href="./tela-cadastro.php">Registre-se Aqui.</a></h4>
-       
-        <div class="form-group">
-                <input type="email" id="email" class="form-control" placeholder="Email:" name="email" onblur="verificarEmail()" title="Escreva o seu Email" required>
-                <span id="mensagem-erro-email"></span>
+        <div class="estrutura">
+            <div class="login-form">
+                <h2 class="titulo-login">Login</h2>
+                <h4>Não tem login? <a href="./tela-cadastro.php">Registre-se Aqui.</a></h4>
+
+                <div class="form-group">
+                    <input type="email" id="email" class="form-control" placeholder="Email:" name="email" onblur="verificarEmail()" title="Escreva o seu Email" required>
+                    <span id="mensagem-erro-email"></span>
+                </div>
+                <div class="form-group" id="form-group-senha">
+                    <input type="password" id="senha" class="form-control" placeholder="Senha:" name="senha" onblur="validaSenha()" title="Escreva a sua Senha escolhida" required>
+                    <span id="erro-senha"></span>
+                    <i class="bi bi-eye-fill" id="btn-senha" onclick="mostrarSenha()"></i>
+                </div>
+                <h4 class="titulo-login">Esqueceu sua senha? <a href="./tela-alterar-senha.php">Clique Aqui.</a></h4>
+                <div class="form-group">
+                    <button type="submit">Entrar</button>
+                </div>
             </div>
-            <div class="form-group" id="form-group-senha">
-                <input type="password" id="senha" class="form-control" placeholder="Senha:" name="senha" onblur="validaSenha()" title="Escreva a sua Senha escolhida" required>
-                <span id="erro-senha"></span>
-                <i class="bi bi-eye-fill" id="btn-senha" onclick="mostrarSenha()"></i>
-            </div>
-            <h4 class="titulo-login">Esqueceu sua senha? <a href="./tela-alterar-senha.php">Clique Aqui.</a></h4>
-            <div class="form-group">
-                <button type="submit">Entrar</button>
-            </div>
-    </div>
     </form>
 
-</div>
-<script src="./javaScript/valid-cad.js"></script>
-<script src="./javaScript/VerSenha.js"></script>
-    
+    </div>
+    <script src="./javaScript/valid-cad.js"></script>
+    <script src="./javaScript/VerSenha.js"></script>
+
 </body>
+
 </html>
