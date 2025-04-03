765c4639e0e4c6c549e3b02b3d1331c86e18de28
caua <gustavo.senac20@gmail.com>
2025-04-03 11:57:33 -0300

    COMIT GUSTAAVO FOOTER

--- INCLUDES/footer.php
+++ INCLUDES/footer.php
@@ -1,38 +1,37 @@
 <img src="./ASSETS/img/tela inicial/rodape.png" alt="Rodapé" class="img-rodape-02">
 
-<footer>
-    <section class="info">
-        <div class="social-media">
-            <h5>REDES SOCIAIS</h5>
-            <a href="https://www.instagram.com/" class="social-icon"><i class="bi bi-instagram"></i> Instagram</a>
-            <a href="https://www.google.com.br/" class="social-icon"><i class="bi bi-google"></i> Google</a>
-            <a href="https://pt-br.facebook.com/" class="social-icon"><i class="bi bi-facebook"></i> Facebook</a>
+
+<section class="info">
+        <div class="footer-section">
+            <h5>Atendimento Ao Cliente</h5>
+            <h6>Telefone - Pedidos, Suporte Técnico e Dúvidas: +55 (19) 99731-0108</h6>
+            <h3><a href="#" class="simule">Simule um financiamento</a></h3>
+            <h6>Whatsapp - Pedidos, Suporte Técnico e Dúvidas: +55 (19) 99731-0108</h6>
+
         </div>
-
-        <div class="customer-support">
-            <h5>SUPORTE AO CLIENTE</h5>
-            <a href="#" class="support-link"><i class="bi bi-chat-left-dots-fill"></i> Entre em contato</a>
-            <br>
-            <a href="#" class="support-link"><i class="bi bi-telephone-fill"></i> Telefone - Suporte ao cliente e Dúvidas</a>
-            <a href="#" class="support-link"><i class="bi bi-whatsapp"></i> WhatsApp - Suporte ao cliente e Dúvidas</a>
+        <div class="footer-section">
+            <h5>Redes Sociais</h5>
+            <h6><a href="https://www.bmw-motorrad.com.br/pt/public-pool/content-pool/contact.html">Contado</a></h6>
+            <h6><a href="https://www.bmw.com.br/pt/more-bmw/fale-com-a-bmw.html">Fale com a BMW </a></h6>
+            <h6><a href="https://www.bmw-motorrad.com.br/pt/public-pool/content-pool/aviso-legal.html">Aviso legal</a>
+            </h6>
         </div>
-
-        <div class="quick-links">
-            <h5>LINKS RÁPIDOS</h5>
-            <a href="#">Política de privacidade</a>
-            <a href="#">Termos de Uso</a>
-            <a href="#">Política de Devolução e Troca</a>
+        <div class="footer-section">
+            <h5>Links Rápidos</h5>
+            <h6><a href="https://www.bmwriderexperience.com.br/">BMW Rider Experience</a></h6>
+            <h6><a
+                    href="https://www.bmwselect.com.br/?utm_medium=produto&utm_source=motorrad&utm_campaign=simule-seu-financiamento">Simule
+                    seu Financiamento</a></h6>
+            <h6><a href="https://www.bmwgroup.com/en.html">BMW Group </a></h6>
         </div>
-
-        <div class="newsletter">
-            <h5>NOTÍCIAS E ATUALIZAÇÕES</h5>
-            <p>Inscreva-se agora com o e-mail para receber as novidades da loja em tempo real!</p>
-            <input type="email" placeholder="Digite seu e-mail" class="email-input">
-            <button class="submit-btn">Enviar</button>
-
+        <div class="footer-section">
+            <h5>Novos Produtos</h5>
+            <h6><a href="https://www.bmw.com.br/pt/index.html">BMW Carros </a></h6>
+            <h6><a href="https://www.bmw-motorrad.com.br/pt/service/financial-service/financing.html">BMW Serviços
+                    Financeiros</a></h6>
+            <h6><a href="https://www.bmw.com.br/pt_BR/topics/fascination-bmw/visita-planta-bmw.html">Visita à
+                    fábrica</a></h6>
         </div>
     </section>
-</footer>
-
 <script src="./javaScript/icopesquisa.js"></script>
 <script src="./javaScript/offCanvas.js"></script>
